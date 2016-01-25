e8d0c9d186731fdbf2e4ad895a5da30b25004e21
James Myers <jmyers@pivotal.io>
2016-01-25 12:46:53 -0800

    Finish dummy implementation of client

--- client.go
+++ client.go
@@ -2,7 +2,6 @@ package astroturf
 
 import (
 	"errors"
-	"time"
 
 	"github.com/cloudfoundry-incubator/garden"
 )
@@ -42,30 +41,74 @@ func (c *client) Create(spec garden.ContainerSpec) (garden.Container, error) {
 }
 
 func (c *client) Destroy(handle string) error {
+	if _, ok := c.containers[handle]; !ok {
+		return errors.New("container does not exist")
+	}
+
 	delete(c.containers, handle)
 	return nil
 }
 
 func (c *client) Containers(properties garden.Properties) ([]garden.Container, error) {
-	return nil, nil
+	matchingContainers := []garden.Container{}
+	for _, container := range c.containers {
+		matched := true
+		for k, v := range properties {
+			value, err := container.Property(k)
+			if err != nil {
+				matched = false
+				break
+			}
+
+			if v != value {
+				matched = false
+				break
+			}
+		}
+
+		if matched {
+			matchingContainers = append(matchingContainers, container)
+		}
+	}
+
+	return matchingContainers, nil
 }
 
 func (c *client) BulkInfo(handles []string) (map[string]garden.ContainerInfoEntry, error) {
-	return nil, nil
+	infos := make(map[string]garden.ContainerInfoEntry)
+	for _, handle := range handles {
+		container, ok := c.containers[handle]
+		if !ok {
+			continue
+		}
+
+		info, err := container.Info()
+		infos[handle] = garden.ContainerInfoEntry{info, garden.NewError(err.Error())}
+	}
+
+	return infos, nil
 }
 
 func (c *client) BulkMetrics(handles []string) (map[string]garden.ContainerMetricsEntry, error) {
-	return nil, nil
+	metrics := make(map[string]garden.ContainerMetricsEntry)
+	for _, handle := range handles {
+		container, ok := c.containers[handle]
+		if !ok {
+			continue
+		}
+
+		metric, err := container.Metrics()
+		metrics[handle] = garden.ContainerMetricsEntry{metric, garden.NewError(err.Error())}
+	}
+
+	return metrics, nil
 }
 
 func (c *client) Lookup(handle string) (garden.Container, error) {
-	return nil, nil
-}
+	container, ok := c.containers[handle]
+	if !ok {
+		return nil, errors.New("container does not exist")
+	}
 
-func (c *client) Start() error { return nil }
-
-func (c *client) Stop() {}
-
-func (c *client) GraceTime(container garden.Container) time.Duration {
-	return 1 * time.Second
+	return container, nil
 }
